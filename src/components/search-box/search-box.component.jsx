263e8442134960d287a1656d661b7d32e7ea474e
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-08-01 08:23:25 -0400

    monsters-rolodex

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import "./search-box.styles.css";
+
+const SearchBox = ({ className, placeholder, onChangeHandler}) => {
+    return(
+        <input
+            type="search"
+            className={`search-box ${className}`}
+            placeholder={placeholder}
+            onChange={onChangeHandler}
+        />
+    );
+}
+
+// class SearchBox extends Component {
+//     render() {
+//         return(
+//             <input
+//                 type="search"
+//                 className={`search-box ${this.props.className}`}
+//                 placeholder={this.props.placeholder}
+//                 onChange={this.props.onChangeHandler}
+//           />
+//         );
+//     }
+// }
+
+export default SearchBox;
